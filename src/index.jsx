bcc71460d4de084989824782a5e32fb777120dfb
w3skulls <hectorochoa.bellon@gmail.com>
2019-05-17 22:18:13 +0200

    Init App

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,18 @@
-import App from './components/App'
+//import App from './components/App'
 
 import React from 'react';
 import { render } from 'react-dom';
 
+class Test extends React.Component{
+    render(){
+        //alert("Funcionando")
+        return(
+            <h1>App funcionando !!!</h1>
+            
+        )
+    }
+}
 
-//Render app
-render(<App />, document.getElementById('root'))    
+
+//Render test
+render(<Test/>, document.getElementById('root'))   
